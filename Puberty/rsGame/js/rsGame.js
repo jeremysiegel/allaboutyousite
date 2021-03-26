f4eaacc30ff07a354182532772363a186c483a4d
jeremysiegel <jeremysiegel2@gmail.com>
2021-03-26 16:05:10 -0500

    rs game

--- Puberty/rsGame/js/rsGame.js
+++ Puberty/rsGame/js/rsGame.js
@@ -1,5 +1,4 @@
 import SceneButton from '../../../common/js/sceneButton.js';
-import ToggleButton from '../../../common/js/toggleButton.js';
     
 export default class RSGame extends Phaser.Scene {
   constructor() {
@@ -10,6 +9,11 @@ export default class RSGame extends Phaser.Scene {
 
     this.load.image('labelLines', '../../Puberty/Male-internal/images/label lines.png');
     this.load.image('MRS', '../../Puberty/Male-internal/images/MRS.png');
+    this.load.image('sperm', '../../Puberty/rsGame/images/sperm.png');
+    this.load.spritesheet('spermSpritesheet', '../../Puberty/rsGame/images/sperm2_spritesheet.png', { frameWidth: 9, frameHeight: 69, margin: 10, spacing: 20 });
+    
+   // this.load.spritesheet('spermSpritesheet2', '../../Puberty/rsGame/images/sperm_spritesheet.png', { frameWidth: 104, frameHeight: 277, margin: 10, spacing: 20 });
+
 
     this.load.image('button', '../../common/images/buttons/red_button01.png');
     this.load.image('buttonPressed', '../../common/images/buttons/red_button02.png');
@@ -19,6 +23,19 @@ export default class RSGame extends Phaser.Scene {
   create () {
     resources.maleInternal = this.add.image(450.1, 289.45, 'MRS'); 
     resources.labelLines = this.add.image(435.05, 318.2, 'labelLines');
+    resources.sperm = this.add.image(346, 413, 'sperm').setAngle(60).setScale(0.65);
+
+
+
+    var testicleTween = this.tweens.add({
+      targets: resources.sperm,
+      x: 340,
+      y: 420,
+      ease: 'Quad.easeInOut',      
+      duration: 1500,
+      yoyo: true,
+      repeat: -1
+    });
 
     resources.labels = this.physics.add.group({collideWorldBounds:true});
 
@@ -71,6 +88,17 @@ export default class RSGame extends Phaser.Scene {
       }
     });
 
+    this.anims.create({
+      key: 'swim',
+      frames: this.anims.generateFrameNumbers('spermSpritesheet', { frames: [0, 1, 2] }),
+      frameRate: 8,
+      repeat: -1
+    });
+
+    const cody = this.add.sprite(268.5, 378.8);
+    cody.setScale(0.75);
+    cody.setAngle(173)
+    cody.play('swim');
 
     resources.homeButton = new SceneButton(this, 600, 280, 'Assistant', '14px', '#f9f9f9', 'button', 'buttonPressed', 'Home', 'title');
   }
@@ -89,37 +117,69 @@ var organs = {
     name: 'Epididymis',
     x: 520.7,
     y: 557,
-    next: 'Vas Deferens'
+    next: 'Vas Deferens',
+    tweens: [
+      {
+        x: 358,
+        y: 388.2,
+        ease: 'Power1',      
+        duration: 1500,
+        angle: 230
+      },
+
+      {
+        x: 360,
+        y: 391,
+        ease: 'Quad.easeInOut',      
+        duration: 1000,
+        yoyo: true,
+        repeat: -1,
+        offset: '-=500'
+      }
+    ]
   },
   vdeferens: {
     name: 'Vas Deferens',
     x: 143.2,
     y: 214.2,
-    next: 'Bladder'
+    next: 'Bladder',
+    tweens: [
+      {
+        x: 383.7,
+        y: 400.2,
+        ease: 'Power1',      
+        duration: 1500,
+        angle: -290
+      },
+    ]
   },
   bladder: {
     name: 'Bladder',
     x: 298.2,
     y: 81.7,
-    next: 'Seminal Vesicle'
+    next: 'Seminal Vesicle',
+    tweens: []
   },
   svesicle: {
     name: 'Seminal Vesicle',
     x: 708.2,
     y: 89.7,
-    next: 'Prostate'
+    next: 'Prostate',
+    tweens: []
   },
   prostate: {
     name: 'Prostate',
     x: 650.7,
     y: 454.2,
-    next: 'Urethra'
+    next: 'Urethra',
+    tweens: []
   },
   urethra: {
     name: 'Urethra',
     x: 155.7,
     y: 339.2,
-    next: null
+    next: null,
+    tweens: []
   },
 };
 
@@ -135,11 +195,26 @@ function nextOrgan(scene) {
       break;
     }
   }
+
+  var tweens = scene.tweens.getTweensOf(resources.sperm);
+  if (tweens[0]) {
+    tweens[0].stop();
+  }
   
+  var timeline = scene.tweens.createTimeline({
+    targets: resources.sperm,
+  });
+
+  resources.currentOrgan.tweens.forEach(function(tween) {
+    tween.targets = resources.sperm,
+    timeline.add(tween);
+  })
+
+  timeline.play();
+
   resources.hitBox.destroy();
   resources.hitBox = scene.add.rectangle(resources.currentOrgan.x, resources.currentOrgan.y, 35, 25);
   resources.hitBoxGroup.add(resources.hitBox);
-
 
 }
 
