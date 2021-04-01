0a34aa9a82404288f93a2b62ad65a6d0f6e67055
jeremysiegel <jeremysiegel2@gmail.com>
2021-04-01 17:38:06 -0500

    pregnancy v2

--- Puberty/Pregnancy/js/pregnancy.js
+++ Puberty/Pregnancy/js/pregnancy.js
@@ -8,6 +8,7 @@ export default class Pregnancy extends Phaser.Scene {
   }
 
   preload () {
+    this.load.image('drawn0', '../../Puberty/Pregnancy/images/drawn0.png');
     this.load.image('drawn1', '../../Puberty/Pregnancy/images/pregnancy-1.png');
     this.load.image('drawn2', '../../Puberty/Pregnancy/images/pregnancy-2.png');
     this.load.image('drawn3', '../../Puberty/Pregnancy/images/pregnancy-3.png');
@@ -18,11 +19,16 @@ export default class Pregnancy extends Phaser.Scene {
     this.load.image('drawn8', '../../Puberty/Pregnancy/images/pregnancy-8.png');
     this.load.image('drawn9', '../../Puberty/Pregnancy/images/pregnancy-9.png');
 
+    this.load.image('picture0', '../../Puberty/Pregnancy/images/embryo.png');
     this.load.image('picture1', '../../Puberty/Pregnancy/images/month 1.jpg');
     this.load.image('picture2', '../../Puberty/Pregnancy/images/month 2.jpg');
-    this.load.spritesheet('picture3', '../../Puberty/Pregnancy/images/heart-spritesheet.png', { frameWidth: 436, frameHeight: 503, margin: 0, spacing: 0 });
-    this.load.spritesheet('picture4', '../../Puberty/Pregnancy/images/yawn-spritesheet.png', { frameWidth: 316, frameHeight: 338, margin: 0, spacing: 0 });
-
+    this.load.spritesheet('picture3', '../../Puberty/Pregnancy/images/fingers-spritesheet.png', { frameWidth: 600, frameHeight: 338, margin: 0, spacing: 0 });
+    this.load.image('picture4', '../../Puberty/Pregnancy/images/twins.jpg');
+    this.load.spritesheet('picture5', '../../Puberty/Pregnancy/images/yawn-spritesheet.png', { frameWidth: 316, frameHeight: 338, margin: 0, spacing: 0 });
+    this.load.spritesheet('picture6', '../../Puberty/Pregnancy/images/heart-spritesheet.png', { frameWidth: 436, frameHeight: 503, margin: 0, spacing: 0 });
+    this.load.spritesheet('picture7', '../../Puberty/Pregnancy/images/kick-spritesheet.png', { frameWidth: 600, frameHeight: 338, margin: 0, spacing: 0 });
+    this.load.spritesheet('picture8', '../../Puberty/Pregnancy/images/breathe-spritesheet.png', { frameWidth: 600, frameHeight: 338, margin: 0, spacing: 0 });
+    this.load.image('picture9', '../../Puberty/Pregnancy/images/term.png');
 
     this.load.image('button', '../../common/images/buttons/red_button01.png');
     this.load.image('buttonPressed', '../../common/images/buttons/red_button02.png');
@@ -35,22 +41,45 @@ export default class Pregnancy extends Phaser.Scene {
     resources.scene = this;
 
     this.anims.create({
-      key: 'heart',
-      frames: this.anims.generateFrameNumbers('picture3', { frames: [0, 1, 2, 3, 4, 5] }),
+      key: 'fingers',
+      frames: this.anims.generateFrameNumbers('picture3'),
       frameRate: 10,
       repeat: -1
     });
 
     this.anims.create({
       key: 'yawn',
-      frames: this.anims.generateFrameNumbers('picture4', {}),
+      frames: this.anims.generateFrameNumbers('picture5'),
       frameRate: 10,
       repeat: -1
     });
 
-    resources.pregnancyDrawing = this.add.sprite(855, 400, 'drawn1');
+    this.anims.create({
+      key: 'heart',
+      frames: this.anims.generateFrameNumbers('picture6'),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'kick',
+      frames: this.anims.generateFrameNumbers('picture7'),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'breathe',
+      frames: this.anims.generateFrameNumbers('picture8'),
+      frameRate: 10,
+      repeat: -1
+    });
+
+
+    resources.pregnancyDrawing = this.add.sprite(855, 400, 'drawn0');
 
     resources.pregnancyDrawingNames = {
+      0: 'drawn0',
       1: 'drawn1',
       2: 'drawn2',
       3: 'drawn3',
@@ -62,9 +91,11 @@ export default class Pregnancy extends Phaser.Scene {
       9: 'drawn9'
     }
 
-    resources.pregnancyPicture = this.add.sprite(200, 250, 'picture1');
+    resources.pregnancyPicture = this.add.sprite(350, 250, 'picture0');
+    resources.pregnancyPicture.setScale(400/resources.pregnancyPicture.height)
 
     resources.pregnancyPictureNames = {
+      0: 'picture0',
       1: 'picture1',
       2: 'picture2',
       3: 'picture3',
@@ -84,12 +115,12 @@ export default class Pregnancy extends Phaser.Scene {
         wordWrap: { width: 400, useAdvancedWrap: true }
       }
     );
+    resources.definitionDisplay.setText(resources.definitions['0']);
 
     var month;
-    var i = 1;
-    var monthX = 20
-    for (i = 1; i < 10; i++) {
-      month = this.add.text(monthX, 550, i, 
+    var monthX = 100
+    for (var i = 0; i < 10; i++) {
+      month = this.add.text(monthX, 520, i, 
         {
           fontFamily: 'Assistant',
           fontSize: '50px',
@@ -99,17 +130,27 @@ export default class Pregnancy extends Phaser.Scene {
       monthX += 50;
      
       month.setInteractive({useHandCursor: true});
+
       month.on('pointerup', function() {
         resources.definitionDisplay.setText(resources.definitions[this._text]);
         resources.pregnancyDrawing.destroy();
         resources.pregnancyDrawing = resources.scene.add.sprite(855, 400, resources.pregnancyDrawingNames[this._text]);
 
         resources.pregnancyPicture.destroy();
-        resources.pregnancyPicture = resources.scene.add.sprite(200, 250, resources.pregnancyPictureNames[this._text]).setScale(0.5);
+        resources.pregnancyPicture = resources.scene.add.sprite(350, 250, resources.pregnancyPictureNames[this._text]);
+        var scale = 400/resources.pregnancyPicture.height;
+        resources.pregnancyPicture.setScale(scale);
+
         if (this._text === '3') {
-          resources.pregnancyPicture.play('heart');
-        } else if (this._text === '4') {
+          resources.pregnancyPicture.play('fingers');
+        } else if (this._text === '5') {
           resources.pregnancyPicture.play('yawn');
+        } else if (this._text === '6') {
+          resources.pregnancyPicture.play('heart');
+        } else if (this._text === '7') {
+          resources.pregnancyPicture.play('kick');
+        } else if (this._text === '8') {
+          resources.pregnancyPicture.play('breathe');
         }
       });
       resources.stages.add(month);
--- Puberty/Pregnancy/js/pregnancyStrings.js
+++ Puberty/Pregnancy/js/pregnancyStrings.js
@@ -1,18 +1,19 @@
 export default class PregnancyStrings {
   constructor() {
     
-    var definitions = {
-      1: 'First month',
-      2: 'Second month',
-      3: 'Third month',
-      4: 'Fourth month',
-      5: 'Fifth month',
-      6: 'Sixth month',
-      7: 'Seventh month',
-      8: 'Eighth month',
-      9: 'Ninth month'
+    var strings = {
+      0: 'This is you! (Or at least, it was you.) The sperm and egg came together three days ago, and already you have divided into 8 cells. You are on your way to the womb. You have a lot of growing to do!',
+      1: "First month: You are starting to take shape. Already you can see your head, legs, and arms starting to grow. Is that a tail? Don't worry, it will go away.",
+      2: 'Second month: Here come your eyes, mouth, and ears. That little white ball is actually a yolk sac for food. For the rest of pregnancy, your food is going to come from a special organ called the placenta. It connects you to your mother through the umbilical cord.',
+      3: 'Third month: You are starting to explore by moving your fingers. Moving in the womb is important to develop the muscles you will use the rest of your life.',
+      4: "Fourth month: Looks like you're not alone! Twins can happen a couple different ways. Sometimes two eggs get fertilized by two different sperm. And sometimes one fertilized egg splits very early in pregnancy and becomes two babies. This is called identical twins.",
+      5: "Fifth month: Yawn! Growing is tiring work, and you will spend a lot of time in the womb sleeping.",
+      6: "Sixth month: Your heart started beating all the way back in the second month, but now it is really cooking. It beats about twice as fast now as it will after you are born.",
+      7: 'Seventh month: Bet mom felt that kick! You are moving around a lot. You can also hear what is going on outside and respond to sound and light.',
+      8: 'Eighth month: The lungs are the last part of you to develop, but you try out breathing in the womb. You are so small that you are actually able to breathe the liquid that you live in.',
+      9: "Ninth month: You are all grown and ready to be born! You'll flip upside down to be born head first. Once you are ready there's no stopping you!"
     } 
 
-    return definitions;
+    return strings;
   }
 }
