75738840887a416ac257700264de53c280bdec88
jeremysiegel <jeremysiegel2@gmail.com>
2021-04-11 21:54:01 -0500

    period v3

--- Puberty/Menstruation/js/period.js
+++ Puberty/Menstruation/js/period.js
@@ -9,8 +9,10 @@ export default class Period extends Phaser.Scene {
   preload () {
   //  this.load.spritesheet('periodSpritesheet', '../../Puberty/Menstruation/images/period_spritesheet.png', { frameWidth: 489.4, frameHeight: 489.4, margin: 10, spacing: 20 });
     //this.load.image('hormonesGraph', '../../Puberty/Menstruation/images/hormones.png');
-    this.load.spritesheet('topSpritesheet', '../../Puberty/Menstruation/images/top sprite.png', { frameWidth: 1200, frameHeight: 277, margin: 0, spacing: 0 });
-    this.load.spritesheet('leftSpritesheet', '../../Puberty/Menstruation/images/left sprite.png', { frameWidth: 480, frameHeight: 858, margin: 0, spacing: 0 });
+   // this.load.spritesheet('topSpritesheet', '../../Puberty/Menstruation/images/top sprite.png', { frameWidth: 1200, frameHeight: 277, margin: 0, spacing: 0 });
+   // this.load.spritesheet('leftSpritesheet', '../../Puberty/Menstruation/images/left sprite.png', { frameWidth: 480, frameHeight: 858, margin: 0, spacing: 0 });
+    this.load.spritesheet('topSpritesheet', '../../Puberty/Menstruation/images/top3 sprite.png', { frameWidth: 300, frameHeight: 69.25, margin: 0, spacing: 0 });
+    this.load.spritesheet('leftSpritesheet', '../../Puberty/Menstruation/images/left3 sprite.png', { frameWidth: 120, frameHeight: 214.5, margin: 0, spacing: 0 });
 
     //  this.load.spritesheet('leftSpritesheet', '../../Puberty/Menstruation/images/left sprite.png', { frameWidth: 395, frameHeight: 664, margin: 0, spacing: 0 });
    // this.load.spritesheet('rightSpritesheet', '../../Puberty/Menstruation/images/right2 sprite.png', { frameWidth: 500, frameHeight: 858, margin: 0, spacing: 0 });
@@ -23,36 +25,35 @@ export default class Period extends Phaser.Scene {
   }
 
   create () {
-   // resources.stage = this.add.sprite(321.3, 277.3, 'periodSpritesheet', 0);
-    //resources.hormonesGraph = this.add.image(990, 200, 'hormonesGraph').setScale(0.55);
-    
     resources.femaleInternal = this.add.image(404.25, 290.55, 'FRS');
 
-    resources.left = this.add.sprite(366.3, 235, 'leftSpritesheet', 0).setScale(0.125);
+    resources.left = this.add.sprite(366.3, 235, 'leftSpritesheet', 0).setScale(0.5);
     resources.right = this.add.sprite(442.2, 235, 'leftSpritesheet', 0);
     resources.right.flipX = true;
-    resources.right.setScale(0.125);
-    resources.top = this.add.sprite(406, 184, 'topSpritesheet', 0).setScale(0.125);
+    resources.right.setScale(0.5);
+    resources.top = this.add.sprite(406, 184, 'topSpritesheet', 0).setScale(0.5);
 
     resources.uterus = this.add.image(404.65, 248.6, 'uterusMask');
     
-    resources.backgroundCircle = this.add.circle(970, 300, 200, 0xcfcffa).setStrokeStyle(5, 0xa3a3c4, 1);
+    resources.backgroundCircle = this.add.circle(970, 300, 200, 0xcfcffa).setStrokeStyle(5, 0x8e4ab3, 0.7);
+    resources.dayLabel = this.add.text(resources.backgroundCircle.x, 360, 'Day 1', 
+    {
+      fontFamily: 'Assistant',
+      fontSize: '25px',
+      fill: '#000'
+    }
+  ).setOrigin(0.5, 0);
+
     var circle = new Phaser.Geom.Circle(970, 300, 170);
-    
     resources.circleGroup = this.add.group(); 
 
     var fillColor = '0xff0000';
     for (var i = 1; i < 29; i++) {
       var dayCircle = this.add.circle(0, 0, 14, fillColor);
 
-      if (i === 1) {
-        dayCircle.setStrokeStyle(4, '0xffda61');
-      } 
-      
       var day = this.add.text(0, 0, i, {fontFamily: 'Arial'}).setOrigin(0.5);
 
       var container = this.add.container(0, 0, [dayCircle, day]);
-
       container
         .setSize(dayCircle.width, dayCircle.height)
         .setInteractive({useHandCursor:true})
@@ -78,7 +79,9 @@ export default class Period extends Phaser.Scene {
       } 
     }
 
-    resources.stage = this.add.text(circle.x, circle.y, 'Period', 
+    Phaser.Actions.PlaceOnCircle(resources.circleGroup.getChildren(), circle, Phaser.Math.DegToRad(-90), Phaser.Math.DegToRad(270));
+
+    resources.stage = this.add.text(circle.x, circle.y - 20, 'Period', 
       {
         fontFamily: 'Assistant',
         fontSize: '50px',
@@ -87,11 +90,11 @@ export default class Period extends Phaser.Scene {
       }
     ).setOrigin(0.5);
 
-    Phaser.Actions.PlaceOnCircle(resources.circleGroup.getChildren(), circle, Phaser.Math.DegToRad(-90), Phaser.Math.DegToRad(270));
-
     resources.backButton = new SceneButton(this, 1200, 567, 0.1, 'periodTitle', 'backButton');
     resources.scene = this;
+
     newEgg();
+    changeDay(1);
   }
 }
 
@@ -104,9 +107,11 @@ function changeDay(day) {
   resources.previousDay = resources.currentDay;
   resources.currentDay = day;
 
-  resources.circleGroup.getChildren()[resources.previousDay-1].list[0].setStrokeStyle(0);
-  resources.circleGroup.getChildren()[day-1].list[0].setStrokeStyle(4, '0xffda61');
+  resources.dayLabel.setText('Day ' + day);
 
+  resources.circleGroup.getChildren()[resources.previousDay-1].list[0].setStrokeStyle(0);
+  resources.circleGroup.getChildren()[day - 1].list[0].setStrokeStyle(4.5, '0xF1F227');
+  
   generateAnimation(resources.previousDay, resources.currentDay);
 
   if (((resources.previousDay === 14 || resources.previousDay === 15) && (resources.currentDay !== 14 && resources.currentDay !== 15)) || (day > 15 && resources.egg)) {
