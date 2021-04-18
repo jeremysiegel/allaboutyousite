043d5014c07d84cdc8c40889cce139d9bba080e0
jeremysiegel <jeremysiegel2@gmail.com>
2021-04-17 22:02:05 -0500

    fixed sp

--- Puberty/External/js/maleExternalStrings.js
+++ Puberty/External/js/maleExternalStrings.js
@@ -7,7 +7,7 @@ export default class maleExternalStrings {
       shaft: 'The shaft holds up the glans of the penis. It can fill with blood and becomes erect, usually during sexual arousal.',
       scrotum: 'The scrotum holds the testicles. It changes size based on the temperature.',
       anus: 'The anus is where feces leaves the body.',
-      foreskin: 'The foreskin is a sleeve of skin that covers the penis. It is present in uncircumcised males, and removed in circumcsed males.'
+      foreskin: 'The foreskin is a sleeve of skin that covers the penis. It is present in uncircumcised males, and removed in circumcised males.'
     } 
 
     var questions = [
