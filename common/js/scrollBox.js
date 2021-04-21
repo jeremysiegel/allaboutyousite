62a029ada2625e278b727291cd795ce2d6cdefc9
jeremysiegel <jeremysiegel2@gmail.com>
2021-04-21 16:20:51 -0500

    fix attempt for scrollbox glitch on mobile

--- common/js/scrollBox.js
+++ common/js/scrollBox.js
@@ -44,8 +44,8 @@ export default class ScrollBox {
     var zone = scene.add.zone(x, y - 3, width, height + 6).setOrigin(0).setInteractive({useHandCursor: true});
 
     zone.on('pointermove', function (pointer) {
-      if (pointer.isDown) {
-        text.y += Math.sign(pointer.velocity.y)*0.08;/*(pointer.velocity.y / 10);*/
+      if (pointer.isDown && pointer.distance > 20) {
+        text.y += (pointer.velocity.y / 10);
         text.y = Phaser.Math.Clamp(text.y, minY, y + 20);
       }
     });
