cabdfedd2bd0e555e09b26f1f42f3ff96a6b996c
jeremysiegel <jeremysiegel2@gmail.com>
2021-04-21 19:19:58 -0500

    progress bar

--- setup/setup.js
+++ setup/setup.js
@@ -9,6 +9,52 @@ export default class Setup extends Phaser.Scene {
   }
 
   preload () {
+    /*Loading progress bar. From Zenva.*/
+    var width = this.cameras.main.width;
+    var height = this.cameras.main.height;
+    
+    var progressBar = this.add.graphics();
+    var progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect((width / 2) - 160, height / 2 + 20, 320, 50);
+
+    var loadingText = this.make.text({
+        x: width / 2,
+        y: height / 2 - 50,
+        text: 'Loading...',
+        style: {
+            font: '30px monospace',
+            fill: '#ffffff'
+        }
+    });
+    loadingText.setOrigin(0.5, 0.5);
+    
+    var percentText = this.make.text({
+        x: width / 2,
+        y: height / 2 - 5,
+        text: '0%',
+        style: {
+            font: '24px monospace',
+            fill: '#ffffff'
+        }
+    });
+
+    percentText.setOrigin(0.5, 0.5);
+    
+    this.load.on('progress', function (value) {
+      percentText.setText(parseInt(value * 100) + '%');
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect((width / 2) - 150, (height / 2) + 30, 300 * value, 30);
+    });
+
+    this.load.on('complete', function () {
+        progressBar.destroy();
+        progressBox.destroy();
+        loadingText.destroy();
+        percentText.destroy();
+    });
+
     /* Some scenes may use images that were loaded for other scenes*/
     
     // common
