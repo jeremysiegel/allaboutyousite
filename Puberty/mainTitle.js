b1547f2837e281ec42348710c4bf9e4e932b6b9e
jeremysiegel <jeremysiegel2@gmail.com>
2021-04-21 20:07:48 -0500

    progress bar

--- Puberty/mainTitle.js
+++ Puberty/mainTitle.js
@@ -42,7 +42,7 @@ export default class MainTitle extends BaseScene {
 
     this.add.image(194.5, 165.25, 'titleText');
 
-    new TitleButton(this, this.cameras.main.width/2 - width/2, this.cameras.main.height/2-height-spacing, width, height, sceneButtons.changesButton.color, sceneButtons.changesButton.text, sceneButtons.changesButton.nextScene);
+    new TitleButton(this, this.cameras.main.width/2 - width/2, this.cameras.main.height/2-height-spacing, width, height + 20, sceneButtons.changesButton.color, sceneButtons.changesButton.text, sceneButtons.changesButton.nextScene);
     new TitleButton(this, this.cameras.main.width/2-width-spacing, this.cameras.main.height/2+spacing, width, height, sceneButtons.reproductiveButton.color, sceneButtons.reproductiveButton.text, sceneButtons.reproductiveButton.nextScene);
     new TitleButton(this, this.cameras.main.width/2+spacing, this.cameras.main.height/2+spacing, width, height, sceneButtons.periodButton.color, sceneButtons.periodButton.text, sceneButtons.periodButton.nextScene);
 
